e4cf18d83959bcf8217b06cc0cd37b35b524ca34
kaitonoda <kaito.noda19@gmail.com>
2023-05-24 13:17:39 +0900

    拡張子変更によってできたエラーの修正

--- lib/posts.ts
+++ lib/posts.ts
@@ -4,29 +4,28 @@ import matter from 'gray-matter';
 import { remark } from 'remark';
 import html from 'remark-html';
 
+type PostData ={
+  id: string;
+  date?:string;
+  [key: string]: any;
+}
+
 const postsDirectory = path.join(process.cwd(), 'posts');
 
 export function getSortedPostsData() {
-  // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory);
-  const allPostsData = fileNames.map((fileName) => {
-    // Remove ".md" from file name to get id
+  const allPostsData:PostData[] = fileNames.map((fileName) => {
     const id = fileName.replace(/\.md$/, '');
-
-    // Read markdown file as string
     const fullPath = path.join(postsDirectory, fileName);
     const fileContents = fs.readFileSync(fullPath, 'utf8');
-
-    // Use gray-matter to parse the post metadata section
     const matterResult = matter(fileContents);
 
-    // Combine the data with the id
     return {
       id,
       ...matterResult.data,
     };
   });
-  // Sort posts by date
+  
   return allPostsData.sort((a, b) => {
     if (a.date < b.date) {
       return 1;
