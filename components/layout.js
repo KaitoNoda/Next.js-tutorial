e0e45f3d69c22eaae0ee16595d770a196ca75aee
kaitonoda <kaito.noda19@gmail.com>
2023-05-25 13:38:11 +0900

    homeをisHomeに変更しBoolean値と分かりやすした

--- components/layout.js
+++ components/layout.js
@@ -7,7 +7,7 @@ import Link from 'next/link';
 const name = 'Kaito Noda';
 export const siteTitle = 'Next.js Website';
 
-export default function Layout({ children, home }) {
+export default function Layout({ children, isHome }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -26,7 +26,7 @@ export default function Layout({ children, home }) {
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <header className={styles.header}>
-        {home ? (
+        {isHome ? (
           <>
             <Image
               priority
@@ -59,7 +59,7 @@ export default function Layout({ children, home }) {
         )}
       </header>
       <main>{children}</main>
-      {!home && (
+      {!isHome && (
         <div className={styles.backToHome}>
           <Link href="/">← Back to home</Link>
         </div>
